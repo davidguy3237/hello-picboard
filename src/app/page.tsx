ebad736eefe4d794d1e829a63b83e064fe00a6d2
David Tran <davidtran3237@gmail.com>
2024-01-19 22:17:55 -0800

    setup initial login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <Button>Button</Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-lg text-white">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
