8fc3ea5ee2e0309662753e5ce90c5c62557303e8
David Tran <davidtran3237@gmail.com>
2024-02-03 16:28:03 -0800

    move dialog into image-card to have img as trigger

--- src/components/image-card.tsx
+++ src/components/image-card.tsx
@@ -1,9 +1,9 @@
 "use client";
 /* eslint-disable @next/next/no-img-element */
-import { ArrowUpRightFromSquare, Maximize } from "lucide-react";
+import { ArrowUpRightFromSquare } from "lucide-react";
 import { Button } from "./ui/button";
-import { SyntheticEvent, useEffect, useState } from "react";
-import { ImageModal } from "./image-modal";
+import { SyntheticEvent } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 interface ImageProps {
   imageUrl: string;
 }
@@ -21,16 +21,47 @@ export function ImageCard({ imageUrl }: ImageProps) {
   return (
     <div className="relative m-1 flex h-fit w-full items-center justify-center overflow-hidden rounded-sm bg-secondary md:h-96 md:w-80">
       <div className="group h-full w-full">
-        <img
-          decoding="async"
-          loading="lazy"
-          alt=""
-          src={imageUrl}
-          className="h-full w-full cursor-pointer object-cover"
+        <Dialog>
+          <DialogTrigger asChild>
+            <img
+              decoding="async"
+              loading="lazy"
+              alt=""
+              src={imageUrl}
+              className="h-full w-full cursor-pointer object-cover"
+              onError={handleOnError}
+            />
+          </DialogTrigger>
+          <DialogContent className="flex h-max max-h-[90%] w-max max-w-[90%] flex-col divide-y overflow-auto xl:h-full xl:max-h-[95%] xl:flex-row xl:divide-x xl:divide-y-0">
+            <img
+              alt=""
+              src={imageUrl}
+              className=" mt-4 max-h-full max-w-full self-center object-contain xl:mt-0"
+            />
+            <div className="w-full flex-shrink-0 pt-2 xl:w-96 xl:pl-4 xl:pt-0">
+              <h1>Header Stuff</h1>
+              <p>Uploaded By</p>
+              <p>Date</p>
+              <p>Dimensions</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                quis dolorum vero, optio enim quod doloribus expedita atque
+                deleniti? Possimus nemo magnam quas consequatur consequuntur
+                magni iure, enim et voluptate sit? Quod provident earum veniam
+                neque quos, officiis illo laboriosam repudiandae placeat quis?
+                Fugit tempora doloribus delectus tempore amet ullam.
+              </p>
+            </div>
+          </DialogContent>
+        </Dialog>
+        <Button
+          className="absolute bottom-0 right-0 hidden rounded-full text-white xl:group-hover:flex xl:group-hover:items-center xl:group-hover:justify-center"
+          variant="ghost"
+          size="icon"
           onClick={onClick}
-          onError={handleOnError}
-        />
-        <ImageModal imageUrl={imageUrl} />
+        >
+          <ArrowUpRightFromSquare />
+        </Button>
       </div>
     </div>
   );
