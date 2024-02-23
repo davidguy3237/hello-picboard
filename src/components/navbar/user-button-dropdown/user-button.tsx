ef7f34e0b13b6d901f9f010ba5415f10ddc03a95
David Tran <davidtran3237@gmail.com>
2024-02-23 04:10:36 -0800

    move settings component into userbutton dropdown

--- src/components/navbar/user-button-dropdown/user-button.tsx
+++ src/components/navbar/user-button-dropdown/user-button.tsx
@@ -2,7 +2,6 @@
 
 import { LogoutButton } from "@/components/auth/logout-button";
 import { DarkModeToggle } from "@/components/navbar/user-button-dropdown/dark-mode-toggle";
-import { SettingsButton } from "@/components/navbar/user-button-dropdown/settings-button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
@@ -12,7 +11,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ExtendedUser } from "@/next-auth";
-import { LogOut, User } from "lucide-react";
+import { Album, Images, LogOut, Settings, User } from "lucide-react";
+import Link from "next/link";
 
 interface UserButtonProps {
   user?: ExtendedUser | undefined;
@@ -30,7 +30,24 @@ export function UserButton({ user }: UserButtonProps) {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-48" align="end">
-        <SettingsButton />
+        <Link href="/test" prefetch={false}>
+          <DropdownMenuItem>
+            <Images className="mr-2 h-4 w-4" />
+            My Posts
+          </DropdownMenuItem>
+        </Link>
+        <Link href="/test" prefetch={false}>
+          <DropdownMenuItem>
+            <Album className="mr-2 h-4 w-4" />
+            My Albums
+          </DropdownMenuItem>
+        </Link>
+        <Link href="/settings" prefetch={false}>
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            Settings
+          </DropdownMenuItem>
+        </Link>
         <DarkModeToggle />
         <DropdownMenuSeparator />
         <LogoutButton>
