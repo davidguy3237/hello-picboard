c8898111f2715117b9b443d250322ed7926ce877
David Tran <davidtran3237@gmail.com>
2024-02-24 00:49:21 -0800

    wrap children in suspense to fix useSearchParams error

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
-export default function AuthLayout({
+import { Suspense } from "react";
+
+export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <div className="flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      {children}
+      <Suspense>{children}</Suspense>
     </div>
   );
 }
