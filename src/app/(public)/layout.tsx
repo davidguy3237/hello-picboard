c8e302fd7f953eab78b01758e2dbc14ced624f75
David Tran <davidtran3237@gmail.com>
2024-02-25 15:28:17 -0800

    moved auth intercept modals into their own folder

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,14 +3,20 @@ import { Navbar } from "@/components/navbar/navbar";
 interface PublicLayoutProps {
   children: React.ReactNode;
   modal: React.ReactNode;
+  auth: React.ReactNode;
 }
 
-export default function PublicLayout({ children, modal }: PublicLayoutProps) {
+export default function PublicLayout({
+  children,
+  modal,
+  auth,
+}: PublicLayoutProps) {
   return (
     <div className="relative flex h-full w-full flex-col items-center">
       <Navbar />
       {children}
       {modal}
+      {auth}
     </div>
   );
 }
