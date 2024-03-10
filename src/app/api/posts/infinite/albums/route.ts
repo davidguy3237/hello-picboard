9445c336fd49e05414f6e028f93907ae0ff37837
David Tran <davidtran3237@gmail.com>
2024-03-10 01:14:54 -0800

    fix prisma error with infinite scroll

--- src/app/api/posts/infinite/albums/route.ts
+++ src/app/api/posts/infinite/albums/route.ts
@@ -11,7 +11,12 @@ export async function GET(req: NextRequest) {
     return NextResponse.json("Album not found", { status: 404 });
   }
 
-  const posts = await db.$queryRaw`
+  // Same issue as /api/posts/infinite/favorites
+
+  let posts;
+
+  if (cursorId && cursorDate) {
+    posts = await db.$queryRaw`
   SELECT post.id, 
   post.public_id AS "publicId", 
   post.user_id AS "userId", 
@@ -22,17 +27,37 @@ export async function GET(req: NextRequest) {
   post.height, 
   post.created_at AS "createdAt", 
   post.updated_at AS "updatedAt", 
-  json_object_agg('postAddedToAlbumDate', posts_albums.created_at) as album,
-  json_agg(json_build_object('postId', favorites.post_id, 'userId', favorites.user_id, 'createdAt', favorites.created_at)) AS favorites
+  json_object_agg('postAddedToAlbumDate', posts_albums.created_at) as album
   FROM post
-  LEFT JOIN favorites ON post.id = favorites.post_id
   JOIN posts_albums ON post.id = posts_albums.post_id
   WHERE posts_albums.album_id = ${albumId}
-  AND (${cursorId}::text IS NULL OR (posts_albums.created_at < ${cursorDate} AND post.id > ${cursorId}))
+  AND posts_albums.created_at < ${cursorDate}::timestamp 
+  AND post.id > ${cursorId}
   GROUP BY post.id
   ORDER BY MAX(posts_albums.created_at) DESC, post.id DESC
   LIMIT 25
   `;
+  } else {
+    posts = await db.$queryRaw`
+  SELECT post.id, 
+  post.public_id AS "publicId", 
+  post.user_id AS "userId", 
+  post.source_url AS "sourceUrl", 
+  post.thumbnail_url AS "thumbnailUrl", 
+  post.description, 
+  post.width, 
+  post.height, 
+  post.created_at AS "createdAt", 
+  post.updated_at AS "updatedAt", 
+  json_object_agg('postAddedToAlbumDate', posts_albums.created_at) as album
+  FROM post
+  JOIN posts_albums ON post.id = posts_albums.post_id
+  WHERE posts_albums.album_id = ${albumId}
+  GROUP BY post.id
+  ORDER BY MAX(posts_albums.created_at) DESC, post.id DESC
+  LIMIT 25
+  `;
+  }
 
   return NextResponse.json(
     { posts },
@@ -41,3 +66,27 @@ export async function GET(req: NextRequest) {
     },
   );
 }
+
+// THIS KEEPS THROWING PRISMA ERRORS (investigate?)
+// code: 22P03
+// message: 'ERROR: incorrect binary data format in bind parameter 3
+// const posts = await db.$queryRaw`
+// SELECT post.id,
+// post.public_id AS "publicId",
+// post.user_id AS "userId",
+// post.source_url AS "sourceUrl",
+// post.thumbnail_url AS "thumbnailUrl",
+// post.description,
+// post.width,
+// post.height,
+// post.created_at AS "createdAt",
+// post.updated_at AS "updatedAt",
+// json_object_agg('postAddedToAlbumDate', posts_albums.created_at) as album
+// FROM post
+// JOIN posts_albums ON post.id = posts_albums.post_id
+// WHERE posts_albums.album_id = ${albumId}
+// AND (${cursorId}::text IS NULL OR (posts_albums.created_at < ${cursorDate} AND post.id > ${cursorId}))
+// GROUP BY post.id
+// ORDER BY MAX(posts_albums.created_at) DESC, post.id DESC
+// LIMIT 25
+// `;
