f325d74966cc1136459186eebfea1d470d1adb03
David Tran <davidtran3237@gmail.com>
2024-03-10 16:03:11 -0700

    remove links and comments

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,12 +1,3 @@
-// TODO: Figure out how to use AWS SES later
-// https://www.npmjs.com/package/@aws-sdk/client-ses
-// https://docs.aws.amazon.com/ses/latest/dg/setting-up.html
-// https://github.com/aws/aws-sdk-js-v3#getting-started
-// https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/javascriptv3/example_code/ses/src/ses_sendemail.js#L15
-// https://stackoverflow.com/questions/73130039/whats-the-up-to-date-way-of-sending-emails-with-aws-ses
-// https://www.suprsend.com/post/how-to-implement-email-sending-in-next-js-with-aws-ses
-// https://medium.com/nerd-for-tech/sending-emails-with-nextjs-and-amazon-simple-email-services-ses-8e4e10d1d397
-
 import { generateTwoFactorToken } from "@/lib/tokens";
 import { Resend } from "resend";
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
