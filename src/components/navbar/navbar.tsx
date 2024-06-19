e5b12d26f6804a0d80915f84c030275f1159f5f5
David Tran <davidtran3237@gmail.com>
2024-06-19 02:03:40 -0700

    keep buttons visible when logged out, but require login when clicked

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,6 +5,7 @@ import { UploadButton } from "@/components/navbar/upload-button";
 import { UserButton } from "@/components/navbar/user-button-dropdown/user-button";
 import { Button } from "@/components/ui/button";
 import { currentUser } from "@/lib/auth";
+import { Upload } from "lucide-react";
 import Link from "next/link";
 
 interface NavbarProps {
@@ -16,7 +17,20 @@ export async function Navbar({ showSearch = true }: NavbarProps) {
     <nav className="sticky top-0 z-20 flex w-full items-center gap-x-2 border-b bg-background p-2">
       <div className="flex flex-1 gap-x-2 lg:min-w-fit">
         <HomeButton />
-        {user && <UploadButton />}
+        {user ? (
+          <UploadButton />
+        ) : (
+          <Link href="/upload" prefetch={false}>
+            <Button
+              variant="outline"
+              aria-label="Upload"
+              className=" h-10 w-10 items-center justify-center p-0 active:bg-background lg:w-auto lg:space-x-2 lg:p-2"
+            >
+              <Upload size={18} />
+              <span className="hidden lg:block">Upload</span>
+            </Button>
+          </Link>
+        )}
       </div>
       {showSearch && <Search />}
       <div className="flex flex-1 justify-end gap-x-2">
